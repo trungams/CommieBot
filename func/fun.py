649cc1e285e481887acb1e0d78e66a1f5f1722f2
trungams <vttrung12@gmail.com>
2018-04-12 23:52:04 -0400

    Added thicc function to fun

--- func/fun.py
+++ func/fun.py
@@ -30,10 +30,27 @@ class Fun():
         """
         ａｅｓｔｈｅｔｉｃ
         """
-        aes_dict = {" ":"　","a":"ａ","b":"ｂ","c":"ｃ","d":"ｄ","e":"ｅ","f":"ｆ","g":"ｇ","h":"ｈ","i":"ｉ","j":"ｊ","k":"ｋ","l":"ｌ","m":"ｍ","n":"ｎ","o":"ｏ","p":"ｐ","q":"ｑ","r":"ｒ","s":"ｓ","t":"ｔ","u":"ｕ","v":"ｖ","w":"ｗ","x":"ｘ","y":"ｙ","z":"ｚ","A":"Ａ","B":"Ｂ","C":"Ｃ","D":"Ｄ","E":"Ｅ","F":"Ｆ","G":"Ｇ","H":"Ｈ","I":"Ｉ","J":"Ｊ","K":"Ｋ","L":"Ｌ","M":"Ｍ","N":"Ｎ","O":"Ｏ","P":"Ｐ","Q":"Ｑ","R":"Ｒ","S":"Ｓ","T":"Ｔ","U":"Ｕ","V":"Ｖ","W":"Ｗ","X":"Ｘ","Y":"Ｙ","Z":"Ｚ","1":"１","2":"２","3":"３","4":"４","5":"５","6":"６","7":"７","8":"８","9":"９","0":"０"}
+        aes_dict = {" ":"　","a":"ａ","b":"ｂ","c":"ｃ","d":"ｄ","e":"ｅ","f":"ｆ","g":"ｇ","h":"ｈ","i":"ｉ","j":"ｊ","k":"ｋ","l":"ｌ","m":"ｍ",
+					"n":"ｎ","o":"ｏ","p":"ｐ","q":"ｑ","r":"ｒ","s":"ｓ","t":"ｔ","u":"ｕ","v":"ｖ","w":"ｗ","x":"ｘ","y":"ｙ","z":"ｚ",
+					"A":"Ａ","B":"Ｂ","C":"Ｃ","D":"Ｄ","E":"Ｅ","F":"Ｆ","G":"Ｇ","H":"Ｈ","I":"Ｉ","J":"Ｊ","K":"Ｋ","L":"Ｌ","M":"Ｍ",
+					"N":"Ｎ","O":"Ｏ","P":"Ｐ","Q":"Ｑ","R":"Ｒ","S":"Ｓ","T":"Ｔ","U":"Ｕ","V":"Ｖ","W":"Ｗ","X":"Ｘ","Y":"Ｙ","Z":"Ｚ",
+					"1":"１","2":"２","3":"３","4":"４","5":"５","6":"６","7":"７","8":"８","9":"９","0":"０"}
         aesthetics = "".join([(aes_dict[c] if aes_dict.get(c) else c) for c in message])
         yield from ctx.message.delete()
         yield from ctx.send(aesthetics)
+
+    @commands.command(pass_context=True)
+    @asyncio.coroutine
+    def thicc(self, ctx, *, message):
+        """
+        乇乂丅尺卂 丅卄工匚匚
+        """
+        thicc_dict = {"a":"卂","b":"乃","c":"匚","d":"刀","e":"乇","f":"下","g":"厶","h":"卄",
+						"i":"工","j":"丁","k":"长","l":"乚","m":"从","n":"𠘨","o":"口","p":"尸",
+						"q":"㔿","r":"尺","s":"丂","t":"丅","u":"凵","v":"リ","w":"山","x":"乂","y":"丫","z":"乙"}
+        extra_thicc = "".join([(thicc_dict[c] if thicc_dict.get(c) else c) for c in message.lower()])
+        yield from ctx.message.delete()
+        yield from ctx.send(extra_thicc)
 
     def get_attachment(self, ctx: discord.ext.commands.Context):
         messages = yield from ctx.channel.history(limit=50).flatten()
